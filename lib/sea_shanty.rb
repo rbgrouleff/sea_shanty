6ad84f1878ab21de9e84d2d9c7d1b5b06be4d5ba
Rasmus Bang Grouleff <rasmus@nerdd.dk>
2022-12-17 20:15:33 +0100

    Initial commit

---
+++ lib/sea_shanty.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "sea_shanty/version"
+
+module SeaShanty
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "sea_shanty"
+
+require "minitest/autorun"
