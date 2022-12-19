150d540311dafddaaff61f9e305c27f753b77e5d
Rasmus Bang Grouleff <rasmus@nerdd.dk>
2022-12-19 13:46:27 +0100

    Use frozen strings

--- test/test_sea_shanty.rb
+++ test/test_sea_shanty.rb
@@ -20,6 +20,18 @@ class TestSeaShanty < Minitest::Test
     assert_equal(SeaShanty.configuration.object_id, yielded_object.object_id)
   end
 
+  def test_inject_inserts_identifier_in_list
+    SeaShanty.inject(:faraday)
+    assert_includes(SeaShanty.injected_libraries, :faraday)
+  end
+
+  def test_inject_only_adds_same_library_once
+    identifier = :foo
+    SeaShanty.inject(identifier)
+    SeaShanty.inject(identifier)
+    assert_equal(1, SeaShanty.injected_libraries.select { |i| i == identifier }.length)
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::SeaShanty::VERSION
   end
