034034b0bd6ae7d69a0af558b71310e67589869e
Rasmus Bang Grouleff <rasmus@nerdd.dk>
2022-12-20 12:12:07 +0100

    Add filename and file_path to Request

--- lib/sea_shanty/request.rb
+++ lib/sea_shanty/request.rb
@@ -1,6 +1,7 @@
 # string_literal_frozen: true
 
 require "digest/sha1"
+require "pathname"
 
 module SeaShanty
   class Request
@@ -15,6 +16,16 @@ module SeaShanty
 
     def digest
       Digest::SHA1.hexdigest(method.to_s + url.to_s + body.to_s)
+    end
+
+    def filename
+      "#{digest}.yml"
+    end
+
+    def file_path
+      # Don't assume system directory separator is /
+      url_path = url.path.delete_prefix("/").split("/").join(File::SEPARATOR)
+      Pathname.new(url.hostname).join(url_path, method.to_s, filename)
     end
   end
 end
--- test/sea_shanty/test_request.rb
+++ test/sea_shanty/test_request.rb
@@ -7,7 +7,11 @@ require "sea_shanty/request"
 module SeaShanty
   class TestRequest < Minitest::Test
     def setup
-      @request = Request.new(method: nil, url: nil, headers: nil, body: nil)
+      @method = :get
+      @url = URI::parse("https://example.com/hello")
+      @headers = {}
+      @body = "body"
+      @request = Request.new(method: @method, url: @url, headers: @headers, body: @body)
     end
 
     def test_has_a_method
@@ -27,14 +31,19 @@ module SeaShanty
     end
 
     def test_digest_uses_http_method_url_and_body
-      method = :get
-      url = URI::parse("https://example.com/hello")
-      headers = {}
-      body = "body"
-      request = Request.new(method: method, url: url, headers: headers, body: body)
+      expected_digest = Digest::SHA1.hexdigest(@method.to_s + @url.to_s + @body)
+      assert_equal(expected_digest, @request.digest)
+    end
 
-      expected_digest = Digest::SHA1.hexdigest(method.to_s + url.to_s + body)
-      assert_equal(expected_digest, request.digest)
+    def test_filename
+      assert_equal("#{@request.digest}.yml", @request.filename)
+    end
+
+    def test_file_path
+      expected = Pathname
+        .new(@url.hostname)
+        .join(@url.path.delete_prefix("/").split("/").join(File::SEPARATOR), @method.to_s, @request.filename)
+      assert_equal(expected, @request.file_path)
     end
   end
 end
