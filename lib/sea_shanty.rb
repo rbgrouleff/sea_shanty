ec4c7773e19a572396316ac32402ef3bcffde84b
Rasmus Bang Grouleff <rasmus@nerdd.dk>
2022-12-29 17:35:19 +0100

    Fix failing tests

--- lib/sea_shanty.rb
+++ lib/sea_shanty.rb
@@ -2,6 +2,7 @@
 
 require "sea_shanty/errors"
 require "sea_shanty/configuration"
+require "sea_shanty/faraday"
 require "sea_shanty/version"
 
 module SeaShanty
@@ -15,12 +16,24 @@ module SeaShanty
 
   def configure(&block)
     yield configuration
-    if ENV["SEA_SHANTY_BYPASS"].present?
-      configuration.bypass = TRUE_VALUES.include?(ENV["SEA_SHANTY_BYPASS"].downcase)
-    end
 
-    if ENV["SEA_SHANTY_READONLY"].present?
-      configuration.readonly = TRUE_VALUES.include?(ENV["SEA_SHANTY_READONLY"].downcase)
+    configuration.bypass = configuration_overwrite(ENV["SEA_SHANTY_BYPASS"], configuration.bypass)
+    configuration.readonly = configuration_overwrite(ENV["SEA_SHANTY_READONLY"], configuration.readonly)
+  end
+
+  def intercept(identifier)
+    intercepted_libraries << identifier unless intercepted_libraries.include?(identifier)
+  end
+
+  def intercepted_libraries
+    @intercepted_libraries ||= []
+  end
+
+  def configuration_overwrite(env_var, value)
+    if env_var.nil? || env_var.empty?
+      value
+    else
+      TRUE_VALUES.include?(env_var.downcase)
     end
   end
 end
