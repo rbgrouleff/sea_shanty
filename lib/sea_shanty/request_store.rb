bdfd166d9dce173ed65be86dadc465a74e1e7883
Rasmus Bang Grouleff <rasmus@nerdd.dk>
2023-03-16 11:06:59 +0100

    Refactor finding, loading, and storing request/response

--- lib/sea_shanty/request_store.rb
+++ lib/sea_shanty/request_store.rb
@@ -18,38 +18,31 @@ module SeaShanty
       if configuration.bypass?
         raise ConfigurationError, "Bypass and readonly are both true - please set only one of them." if configuration.readonly?
         yield
-      elsif configuration.readonly?
-        load_response(request)
-      elsif has_response_for?(request)
-        load_response(request)
       else
-        response = yield
-        store(request, response) unless configuration.bypass?
-        response
+        path = request_file_path(request)
+        if configuration.readonly? || path.exist?
+          load_response(path, request)
+        else
+          response = yield
+          store(path, request, response) unless configuration.bypass?
+          response
+        end
       end
     end
 
-    def has_response_for?(request)
-      request_file_path(request).exist?
-    end
-
-    def load_response(request)
-      raise UnknownRequest, "SeaShanty: Unknown request #{request.method.to_s.upcase} to #{request.url}" unless has_response_for?(request)
-      contents = YAML.safe_load(request_file_path(request).read, permitted_classes: [Symbol, Time, DateTime])
+    def load_response(path, request)
+      raise UnknownRequest, "SeaShanty: Unknown request #{request.method.to_s.upcase} to #{request.url}" unless path.exist?
+      contents = YAML.safe_load(Pathname(path).read, permitted_classes: [Symbol, Time, DateTime])
       Response.from_h(contents.fetch(:response))
     end
 
-    def store(request, response)
+    def store(path, request, response)
       file_path = request_file_path(request)
-      file_path.dirname.mkpath
-      file_path.open("w+") do |file|
+      path.dirname.mkpath
+      path.open("w+") do |file|
         file.write(YAML.dump(serialize(request, response)))
       end
     end
-
-    private
-
-    attr_reader :configuration, :generic_responses, :storage_dir, :request_serializer
 
     def request_file_path(request)
       _, generic_file_path = generic_responses.find { |matcher, path| matcher.match?(request.url.to_s) }
@@ -61,6 +54,10 @@ module SeaShanty
 
       storage_dir.join(file_path)
     end
+
+    private
+
+    attr_reader :configuration, :generic_responses, :storage_dir, :request_serializer
 
     def serialize(request, response)
       {
